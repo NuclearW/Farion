1d3b8e99a22f0883cf9bd420b6a14e8cddb2a365
NuclearW <incongruency@gmail.com>
2012-04-09 16:54:14 -0400

    Talking heads

--- src/main/java/com/nuclearw/farion/Bot.java
+++ src/main/java/com/nuclearw/farion/Bot.java
@@ -1,7 +1,28 @@
 package com.nuclearw.farion;
 
+import org.jibble.pircbot.Colors;
 import org.jibble.pircbot.PircBot;
 
 public class Bot extends PircBot {
+	private static Farion plugin;
 
+	public Bot(Farion instance) {
+		plugin = instance;
+
+		this.setName(Config.nick);
+	}
+
+	@Override
+	protected void onConnect() {
+		identify(Config.nickServPassword);
+	}
+
+	@Override
+	protected void onMessage(String channel, String sender, String login, String hostname, String message) {
+		if (channel.equalsIgnoreCase(Config.channel)) {
+			plugin.getServer().broadcastMessage("[IRC] <" + sender + ">: " + Colors.removeFormattingAndColors(message));
+		} else if (channel.equalsIgnoreCase(Config.modChannel)) {
+			// TODO: Mod Channel
+		}
+	}
 }
--- src/main/java/com/nuclearw/farion/Farion.java
+++ src/main/java/com/nuclearw/farion/Farion.java
@@ -1,15 +1,56 @@
 package com.nuclearw.farion;
 
-import org.bukkit.plugin.java.JavaPlugin;
+import java.io.IOException;
 
-public class Farion extends JavaPlugin {
+import javax.net.ssl.SSLException;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jibble.pircbot.IrcException;
+import org.jibble.pircbot.NickAlreadyInUseException;
+import org.jibble.pircbot.TrustingSSLSocketFactory;
+
+public class Farion extends JavaPlugin implements Listener {
+	private static Bot bot;
+
 	@Override
 	public void onEnable() {
 		Config.load(this);
+
+		bot = new Bot(this);
+		try {
+			if(Config.ssl) {
+				bot.connect(Config.hostname, Config.port, new TrustingSSLSocketFactory());
+			} else {
+				bot.connect(Config.hostname, Config.port, null);
+			}
+
+			bot.joinChannel(Config.channel);
+
+			// TODO: Mod Channel
+		} catch (NickAlreadyInUseException e) {
+			e.printStackTrace();
+		} catch (SSLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (IrcException e) {
+			e.printStackTrace();
+		}
+
+		getLogger().info("Finished Loading " + getDescription().getFullName());
 	}
 
 	@Override
 	public void onDisable() {
-		
+		getLogger().info("Finished Unloading "+getDescription().getFullName());
+	}
+
+	@EventHandler(ignoreCancelled = true)
+	public void onChat(PlayerChatEvent event) {
+		bot.sendMessage(Config.channel, "<" + event.getPlayer().getName() + ">: " + event.getMessage());
+		// TODO: Mod Channel
 	}
 }
