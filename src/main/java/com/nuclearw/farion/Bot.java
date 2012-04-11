796ccf48786fbdb305a23d64a7757ff7a2977963
NuclearW <incongruency@gmail.com>
2012-04-11 05:20:29 -0400

    No more double logging I know what it means

--- src/main/java/com/nuclearw/farion/Bot.java
+++ src/main/java/com/nuclearw/farion/Bot.java
@@ -61,7 +61,7 @@ public class Bot extends PircBot {
 			}
 
 			plugin.getServer().broadcastMessage("[IRC] <" + sender + "> " + message);
-			plugin.getLogger().info("[IRC][" + Config.channel + "] <" + sender + "> " + message);
+//			plugin.getLogger().info("[IRC][" + Config.channel + "] <" + sender + "> " + message);
 		} else if(channel.equalsIgnoreCase(Config.modChannel)) {
 			// TODO: Mod Channel
 		}
@@ -71,7 +71,7 @@ public class Bot extends PircBot {
 	protected void onAction(String sender, String login, String hostname, String target, String action) {
 		if(target.equalsIgnoreCase(Config.channel)) {
 			plugin.getServer().broadcastMessage("[IRC] * " + sender + " " + action);
-			plugin.getLogger().info("[IRC] * " + sender + " " + action);
+//			plugin.getLogger().info("[IRC] * " + sender + " " + action);
 		} else if(target.equalsIgnoreCase(Config.modChannel)) {
 			// TODO: Mod channel
 		}
